c826667997a24c995fcd58e457d110f87b1930ab
Reus.Huang <1027559660@qq.com>
2017-09-18 23:10:44 +0800

    Add files via upload

--- AllFootball.py
+++ AllFootball.py
@@ -1,21 +1,72 @@
 import requests
+from lxml import etree
+import os
 import re
+import time
 
-headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.3','Host':'www.dongqiudi.com','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Accept-Encoding':'gzip, deflate','Accept-Language':'zh-CN,zh;q=0.8','Cache-Control':'max-age=0','Connection':'keep-alive'}
-html = requests.get('http://www.dongqiudi.com',headers=headers)
-html.encoding='utf-8'
+headers = {
+
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.3',
+        'Host':'www.dongqiudi.com',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.8',
+        'Cache-Control':'max-age=0',
+        'Connection':'keep-alive',
+        'Upgrade-Insecure-Requests':'1'
+
+           }
+
+def main():
+
+    while True:
+
+        html = requests.get('http://www.dongqiudi.com',headers=headers)
+
+        html.encoding='utf-8'
 
 #print(html.text)
 
-file=open('log.txt','w+')
+        selector=etree.HTML(html.text)
+#file=open('log.txt','w+')
 
-news=re.findall('target="_blank" class="pc_count">(.*?)</a>',html.text)
+#news=re.findall('target="_blank" class="pc_count">(.*?)</a>',html.text)
 
-#file.writelines(news)
+#for each in news:
+#    file.write(each+'\n')
 
-for each in news:
-    file.write(each+'\n')
-    #print(each)
+        now=time.strftime("%Y%m%d%H:%M:%S",time.localtime(time.time()))
+        os.mkdir(now,0o777)
 
-file.close()
+        football_news=selector.xpath('//div[@id="news_list"]/ol/li/h2/a/@href ')
 
+        news_title=selector.xpath('//div[@id="news_list"]/ol/li/h2/a/text()')
+
+#print(news_title)
+#print(football_news)
+
+        BASE_DIR=os.path.dirname(__file__)
+#print(BASE_DIR)
+
+        file_path=os.path.join(BASE_DIR,now)
+#print(file_path)
+
+
+        for i in range(0,len(news_title)):
+            new_file=open(file_path+'/'+news_title[i]+'.txt','w')
+
+            news_html=requests.get(url=football_news[i],headers=headers)
+            news_html.encoding='utf-8'
+
+            selector=etree.HTML(news_html.text)
+            news_content=selector.xpath('//div[@id="main"]/div[@id="con"]/div/div/div/p/text()')
+
+            new_file.writelines(news_content)
+            new_file.close()
+
+        time.sleep(3600)
+#file.close()
+
+
+if __name__=="__main__":
+    main()
